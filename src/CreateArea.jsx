36b2eb4cc6cea4cdfb4907430510a147d4e17adb
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-08-01 17:15:09 +0530

    Keeper App Completed

---
+++ src/CreateArea.jsx
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+
+function CreateArea(props){
+const [note,setNote]=useState({
+  title:"",
+  content:""
+});
+  function handleChange(event){
+    const {name,value}=event.target;
+  setNote(prevNote=>{
+    return {
+      ...prevNote,
+      [name]:value
+    };
+  });
+  }
+  function submitNote(event){
+    props.onAdd(note);
+    setNote({
+      title:"",
+      content:""
+    });
+    event.preventDefault();
+  }
+    return(
+        <div>
+      <form>
+        <input onChange={handleChange} name="title"
+        value={note.title} placeholder='Title'></input>
+        <textarea onChange={handleChange} name="content" value={note.content} rows="3" placeholder='Take a note...'></textarea>
+        <button onClick={submitNote}>Add</button>
+       </form>
+        </div>     
+    )
+    }
+export default CreateArea;
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const date=new Date().getFullYear();
+function Footer(){
+    return(
+        <footer>Copyright ©️ {date} </footer>
+    )
+}
+export default Footer;
